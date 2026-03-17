f184c6860ab2e003294198e2f377b068d5f3e33d
prawar-hash <prawar65@gmail.com>
2026-03-17 15:24:54 +0530

    commit

---
+++ 46.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Recursive function to calculate factorial
+long long factorial(int n)
+{
+  if (n == 0 || n == 1) // Base case
+  return 1;
+  else
+  return n * factorial(n - 1); // Recursive call
+}
+int main()
+{
+  int num;
+  long long result;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  if (num < 0)
+  {
+      printf("Factorial is not defined for negative numbers.\n");
+  } 
+  else
+  {
+      result = factorial(num);
+      printf("Factorial of %d is %lld\n", num, result);
+  }
+  return 0;
+}
